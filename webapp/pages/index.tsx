020693656aa3b4ff4ef05047185cd50431dee117
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-06-20 18:31:11 -0400

    [OWL-2] Add `AppBar` component from Grommet tutorial

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -3,8 +3,29 @@
  * the Open Software License version 3.0.
  */
 
+import { Box } from 'grommet';
+
+const AppBar = (props) => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation="medium"
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return (
+    <>
+      <AppBar>Hello Grommet!</AppBar>
+      <div>Welcome to Next.js!</div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
