eee0cc8787c33001c845dddbe582fa2847bc745e
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-06-19 22:04:15 -0400

    Add Next.js to Webapp

---
+++ webapp/pages/index.tsx
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2021 Phillip Gates-Shannon. All rights reserved. Licensed under
+ * the Open Software License version 3.0.
+ */
+
+function HomePage() {
+  return <div>Welcome to Next.js!</div>
+}
+
+export default HomePage
