d1710b104f62d2490316b95f3513110ea2cd009b
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-07-31 13:20:37 -0400

    [OWL-22] Add `Protect` component

--- webapp/components/index.ts
+++ webapp/components/index.ts
@@ -5,3 +5,4 @@
 
 export { AppBar } from './AppBar';
 export { NavBar } from './NavBar';
+export { Protect } from './Protect';
--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -3,34 +3,12 @@
  * the Open Software License version 3.0.
  */
 
-import { Box, Button } from 'grommet';
-import { useRouter } from 'next/router';
+import { Box } from 'grommet';
 import * as React from 'react';
 
-import { NavBar } from 'components';
-import { LOGIN_PATH, useSession } from 'lib';
+import { NavBar, Protect } from 'components';
 import db from 'server-lib/prisma';
-
-function Content() {
-  const router = useRouter();
-  const { isError, isLoading, session } = useSession();
-
-  if (isError) {
-    return <div>Error!</div>;
-  }
-
-  if (isLoading) {
-    return null;
-  }
-
-  if (session) {
-    return <div>Hello {session.user?.name}</div>;
-  }
-
-  return (
-    <Button label="Login" onClick={() => router.push(LOGIN_PATH)} primary />
-  );
-}
+import { Session } from 'types';
 
 const HomePage: React.FC = () => {
   return (
@@ -38,7 +16,9 @@ const HomePage: React.FC = () => {
       <NavBar />
       <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
         <Box flex align="start" direction="column" justify="start" pad="medium">
-          <Content />
+          <Protect>
+            {(session: Session) => <div>Hello {session.user?.name}</div>}
+          </Protect>
         </Box>
       </Box>
     </>
