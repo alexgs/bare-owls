fb8908dd103f0def856fbc50f7569c0337fdb023
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-08-15 16:08:24 -0400

    [OWL-40] Add example Node.js app from FusionAuth repo

---
+++ fusionauth-example/routes/index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const {FusionAuthClient} = require('@fusionauth/typescript-client');
+
+// tag::clientIdSecret[]
+const clientId = '2323677f-62b9-467f-8cd6-931169f237f9';
+const clientSecret = '4M6wtUAo4tzADucALbD8CrltW60_MrvnaeavRLYQdGg';
+// end::clientIdSecret[]
+
+const client = new FusionAuthClient('noapikeyneeded', 'http://localhost:9011');
+const pkceChallenge = require('pkce-challenge');
+
+/* logout home page. */
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+  res.redirect(302, '/');
+});
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  const stateValue = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  req.session.stateValue = stateValue;
+
+  //generate the pkce challenge/verifier dict
+  const pkce_pair = pkceChallenge();
+  // Store the PKCE verifier in session
+  req.session.verifier = pkce_pair['code_verifier'];
+  const challenge = pkce_pair['code_challenge'];
+  res.render('index', {user: req.session.user, title: 'FusionAuth Example', clientId: clientId, challenge: challenge, stateValue: stateValue});
+});
+
+// tag::fullOAuthCodeExchange[]
+/* OAuth return from FusionAuth */
+router.get('/oauth-redirect', function (req, res, next) {
+  const stateFromServer = req.query.state;
+  if (stateFromServer !== req.session.stateValue) {
+    console.log("State doesn't match. uh-oh.");
+    console.log("Saw: " + stateFromServer + ", but expected: " + req.session.stateValue);
+    res.redirect(302, '/');
+    return;
+  }
+
+// tag::exchangeOAuthCode[]
+  // This code stores the user in a server-side session
+ client.exchangeOAuthCodeForAccessTokenUsingPKCE(req.query.code,
+                                                 clientId,
+                                                 clientSecret,
+                                                 'http://localhost:3000/oauth-redirect',
+                                                 req.session.verifier)
+// end::exchangeOAuthCode[]
+      .then((response) => {
+        console.log(response.response.access_token);
+        return client.retrieveUserUsingJWT(response.response.access_token);
+      })
+      .then((response) => {
+        if (!response.response.user.registrations || response.response.user.registrations.length == 0 || (response.response.user.registrations.filter(reg => reg.applicationId === clientId)).length == 0) {
+          console.log("User not registered, not authorized.");
+          res.redirect(302, '/');
+          return;
+        }
+
+// tag::setUserInSession[]
+        req.session.user = response.response.user;
+      })
+// end::setUserInSession[]
+      .then((response) => {
+        res.redirect(302, '/');
+      }).catch((err) => {console.log("in error"); console.error(JSON.stringify(err));});
+
+  // This code pushes the access and refresh tokens back to the browser as secure, HTTP-only cookies
+  //     .then((response) => {
+  //       res.cookie('access_token', response.response.access_token, {httpOnly: true});
+  //       res.cookie('refresh_token', response.response.refresh_token, {httpOnly: true});
+  //       res.redirect(302, '/');
+  //     }).catch((err) => {console.log("in error"); console.error(JSON.stringify(err));});
+});
+// end::fullOAuthCodeExchange[]
+
+module.exports = router;
