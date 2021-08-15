49450519e2bcd1e11f5a5fddd7afbf8321438d70
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-08-15 19:09:37 -0400

    [OWL-43] Adjust homepage data fetcher to model new schema

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -3,16 +3,43 @@
  * the Open Software License version 3.0.
  */
 
-import { Box } from 'grommet';
+import { Box, Button } from 'grommet';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 
-import { NavBar, RequireLogin } from 'components';
-import { useSession } from 'lib';
+import { NavBar } from 'components';
+import { LOGIN_PATH } from 'lib';
+
+interface Data {
+  payload?: {
+    displayName: string;
+  };
+  status: number;
+}
 
 const Content: React.FC = () => {
-  const session = useSession();
-  return <div>Hello {session.user?.name}</div>;
-};
+  const data: Data = {
+    // payload: {
+    //   displayName: 'Alex',
+    // },
+    // status: 200,
+    status: 409,
+  }
+  const router = useRouter();
+
+  let session = null;
+  if (data) {
+    session = data.status === 200 ? data.payload : null;
+  }
+
+  if (session) {
+    return <div>Hello {session.displayName}</div>;
+  }
+
+  return (
+    <Button label="Login" onClick={() => router.push(LOGIN_PATH)} primary />
+  );
+}
 
 const HomePage: React.FC = () => {
   return (
@@ -20,9 +47,7 @@ const HomePage: React.FC = () => {
       <NavBar />
       <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
         <Box flex align="start" direction="column" justify="start" pad="medium">
-          <RequireLogin>
-            <Content />
-          </RequireLogin>
+          <Content />
         </Box>
       </Box>
     </>
