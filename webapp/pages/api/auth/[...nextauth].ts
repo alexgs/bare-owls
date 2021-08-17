6ee229a9ab3d3be9cf87d3c833ba54fe59c44fd1
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-08-16 21:45:48 -0400

    [OWL-43] Clean up env vars

--- webapp/pages/api/auth/[...nextauth].ts
+++ webapp/pages/api/auth/[...nextauth].ts
@@ -4,23 +4,19 @@ import Providers from 'next-auth/providers';
 
 const FUSIONAUTH_CLIENT_ID = env.get('FUSIONAUTH_CLIENT_ID').required().asString();
 const FUSIONAUTH_CLIENT_SECRET = env.get('FUSIONAUTH_CLIENT_SECRET').required().asString();
-const FUSIONAUTH_DOMAIN = env.get('FUSIONAUTH_DOMAIN').required().asString();
-const WEBAPP_FUSIONAUTH_HOST = env.get('WEBAPP_FUSIONAUTH_HOST').required().asString();
+const FUSIONAUTH_HOST_EXTERNAL = env.get('FUSIONAUTH_HOST_EXTERNAL').required().asString(); // Root URL from outside the Docker network
+const FUSIONAUTH_HOST_INTERNAL = env.get('FUSIONAUTH_HOST_INTERNAL').required().asString(); // Root URL from inside the Docker network
 
-// noinspection HttpUrlsUsage
 export default NextAuth({
   providers: [
     Providers.FusionAuth({
       clientId: FUSIONAUTH_CLIENT_ID,
       clientSecret: FUSIONAUTH_CLIENT_SECRET,
-      domain: FUSIONAUTH_DOMAIN,
 
-      // Override values until TLS is working for FusionAuth
-      authorizationUrl: `http://${FUSIONAUTH_DOMAIN}/oauth2/authorize?response_type=code`,
-
-      // These two are called on the server and need the Docker hostname
-      accessTokenUrl: `http://${WEBAPP_FUSIONAUTH_HOST}/oauth2/token`,
-      profileUrl: `http://${WEBAPP_FUSIONAUTH_HOST}/oauth2/userinfo`,
+      // Override the following values (side effect: no need to set the `domain` field)
+      authorizationUrl: `${FUSIONAUTH_HOST_EXTERNAL}/oauth2/authorize?response_type=code`,
+      accessTokenUrl: `${FUSIONAUTH_HOST_INTERNAL}/oauth2/token`,
+      profileUrl: `${FUSIONAUTH_HOST_INTERNAL}/oauth2/userinfo`,
     }),
   ],
 });
