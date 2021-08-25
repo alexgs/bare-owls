0bc7c1dfc660a73d6d8950ff2cd209f7a1f55569
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-08-24 21:20:01 -0400

    [OWL-35] Add stubs for handling request authentication and token refresh

--- webapp/pages/api/session.ts
+++ webapp/pages/api/session.ts
@@ -3,13 +3,60 @@
  * the Open Software License version 3.0.
  */
 
+import * as cookie from 'cookie';
 import { NextApiRequest, NextApiResponse } from 'next';
+
+import { getConfig, getOidcClient } from 'server-lib';
 
 // TODO Update this file (or maybe just the callback functions?)
 // TODO Pass tokens only in cookies
 
-function handler(req: NextApiRequest, res: NextApiResponse): void {
-  return res.json({ displayName: 'Alex' });
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function isExpired(token: string): boolean {
+  // TODO Actually check for expiration
+  return false;
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  const config = getConfig();
+  const { COOKIE } = config;
+  const cookies: string[] = [];
+
+  const accessToken = req.cookies[COOKIE.ACCESS_TOKEN.NAME];
+  const refreshToken = req.cookies[COOKIE.REFRESH_TOKEN.NAME];
+  if (!refreshToken) {
+    // TODO Redirect to login page
+  } else if (!accessToken || isExpired(accessToken)) {
+    const client = await getOidcClient(config);
+    const tokens = await client.refresh(refreshToken);
+    const newAccessToken = tokens.access_token;
+    const newRefreshToken = tokens.refresh_token;
+
+    if (newAccessToken && newRefreshToken) {
+      const accessTokenCookie = cookie.serialize(
+        COOKIE.ACCESS_TOKEN.NAME,
+        newAccessToken,
+        COOKIE.ACCESS_TOKEN.SET,
+      );
+      cookies.push(accessTokenCookie);
+
+      const refreshTokenCookie = cookie.serialize(
+        COOKIE.REFRESH_TOKEN.NAME,
+        newRefreshToken,
+        COOKIE.REFRESH_TOKEN.SET,
+      );
+      cookies.push(refreshTokenCookie);
+    }
+  } else {
+    // TODO Verify the token signature
+    // TODO Check for token revocation
+  }
+
+  // TODO Get the claims
+  const jwt = accessToken;
+
+  res.setHeader('set-cookie', cookies);
+  res.json({ jwt });
 }
 
 export default handler;
