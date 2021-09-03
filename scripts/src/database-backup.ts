01654f274a0aeca56e73a99572156a4eafb22541
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-09-03 16:20:52 -0400

    [OWL-80] Add check for missing commands

--- scripts/src/database-backup.ts
+++ scripts/src/database-backup.ts
@@ -1,4 +1,24 @@
+import shell from 'shelljs';
+
+const requiredCommands = ['docker-compose', 'psql'];
+
 async function main() {
+  const missingCommands = requiredCommands
+    .map((command) => {
+      if (shell.which(command)) {
+        return undefined;
+      } else {
+        return command;
+      }
+    })
+    .filter((command) => !!command);
+  if (missingCommands.length === 0) {
+    console.log('All required commands are present.');
+  } else {
+    console.log(`The following commands are missing: ${missingCommands.join(', ')}`);
+    shell.exit(2);
+  }
+
   console.log('Hello database backups!');
 }
 
