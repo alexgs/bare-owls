9e310ad89ea9d4952a3a20d7c0d73e4b11373573
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-09-06 11:55:06 -0400

    [OWL-82] Add session data to homepage via server-side props

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -4,25 +4,47 @@
  */
 
 import { Box } from 'grommet';
+import { GetServerSideProps, NextApiRequest, NextApiResponse } from 'next';
 import * as React from 'react';
 
 import { NavBar } from 'components';
+import { STATUS, getAccessToken, getSession } from 'server-lib';
+import { Session } from 'types';
 
-const Content: React.FC = () => {
+interface Props {
+  session: Session | null;
+}
+
+const Content: React.FC<Props> = (props: Props) => {
+  if (props.session) {
+    return <div>Hello {props.session.displayName}!</div>
+  }
   return <div>Hello world</div>;
 };
 
-const HomePage: React.FC = () => {
+const HomePage: React.FC<Props> = (props: Props) => {
   return (
     <>
       <NavBar />
       <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
         <Box flex align="start" direction="column" justify="start" pad="medium">
-          <Content />
+          <Content session={props.session} />
         </Box>
       </Box>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const result = await getAccessToken(
+    context.req as NextApiRequest,
+    context.res as NextApiResponse,
+  );
+  if (result.status === STATUS.OK) {
+    const session = await getSession(result.token);
+    return { props: { session } };
+  }
+  return { props: { session: null } };
 };
 
 export default HomePage;
