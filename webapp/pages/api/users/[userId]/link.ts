6cad86fc820928dbdeae946e414d84e6479a29f6
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-09-10 09:45:18 -0400

    [OWL-118] Add endpoint handler to link a user account to the auth system

--- webapp/pages/api/users/[userId]/link.ts
+++ webapp/pages/api/users/[userId]/link.ts
@@ -6,9 +6,18 @@
 import got from 'got';
 import { NextApiRequest, NextApiResponse } from 'next';
 import join from 'url-join';
+import * as yup from 'yup';
 
 import { PUBLIC } from 'lib';
-import { getConfig } from 'server-lib';
+import { HTTP_CODE, getConfig } from 'server-lib';
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  password: yup.string().required(),
+  username: yup.string().required(),
+});
 
 async function handler(
   req: NextApiRequest,
@@ -35,6 +44,39 @@ async function handler(
     } else {
       throw new Error(`${response.statusCode} ${response.statusMessage ?? ''}`);
     }
+  }
+
+  if (req.method === 'POST') {
+    const { AUTH_API_KEY, AUTH_ORIGIN_INTERNAL } = getConfig();
+    const userinfoEndpoint = join(
+      AUTH_ORIGIN_INTERNAL,
+      `/api/user/${req.query.userId as string}`,
+    );
+    const userinfoPayload = {
+      skipVerification: true,
+      user: await schema.validate(req.body),
+    };
+
+    const response = await got.post(userinfoEndpoint, {
+      json: userinfoPayload,
+      headers: {
+        Authorization: AUTH_API_KEY,
+      },
+      responseType: 'json',
+      throwHttpErrors: false,
+    });
+    // TODO Register with the "Core" auth application
+    if (response.statusCode === 200) {
+      res.json({ linkStatus: PUBLIC.AUTH_LINK.LINKED });
+    } else {
+      throw new Error(`${response.statusCode} ${response.statusMessage ?? ''}`);
+    }
+  }
+
+  if (!['GET', 'POST'].includes(req.method ?? '')) {
+    res
+      .status(HTTP_CODE.BAD_REQUEST)
+      .json({ message: `Unsupported method ${req.method?.toUpperCase() ?? '<NONE>'}` });
   }
 }
 
