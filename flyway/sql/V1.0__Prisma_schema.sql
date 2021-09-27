5d2e3f96a79502983e41a43af8c71339823bef90
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-09-27 08:15:20 -0400

    OWL-142: Add triggers for all `updated_at` columns

--- flyway/sql/V1.0__Prisma_schema.sql
+++ flyway/sql/V1.0__Prisma_schema.sql
@@ -1,7 +1,20 @@
--- Schema adapted from pg_dump output 2021-09-21 8:14:11 EDT. Prior to this
--- time, the schema and seed data was managed by Prisma.
+-- Prior to 2021-09-21, the schema and seed data was managed by Prisma.
 
---[ # TABLE public.channels ]--
+-- TODO Add PGSQL `genUuid` function as default for all UUID PK columns
+
+--[ # TRIGGERS # ]--
+
+-- See https://x-team.com/blog/automatic-timestamps-with-postgresql/
+CREATE OR REPLACE FUNCTION trigger_set_updated_at()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    new.updated_at = NOW();
+    RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+--[ # TABLE public.channels # ]--
 
 CREATE TABLE IF NOT EXISTS public.channels
 (
@@ -11,7 +24,8 @@ CREATE TABLE IF NOT EXISTS public.channels
     slug       TEXT                                                     NOT NULL,
     title      TEXT                                                     NOT NULL,
     owner_id   UUID                                                     NOT NULL
-        CONSTRAINT channels_owner_id_fk REFERENCES user_accounts
+        CONSTRAINT channels_owner_id_fk
+            REFERENCES user_accounts
             ON DELETE CASCADE
             ON UPDATE CASCADE,
     created_at TIMESTAMP(3) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -26,7 +40,13 @@ CREATE UNIQUE INDEX channels_slug_key
 CREATE UNIQUE INDEX channels_owner_id_unique
     ON channels (owner_id);
 
---[ # TABLE public.channel_subscriptions ]--
+CREATE TRIGGER set_channels_updated_at
+    BEFORE UPDATE
+    ON channels
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at();
+
+--[ # TABLE public.channel_subscriptions # ]--
 
 CREATE TABLE IF NOT EXISTS public.channel_subscriptions
 (
@@ -44,7 +64,8 @@ CREATE TABLE IF NOT EXISTS public.channel_subscriptions
             ON DELETE CASCADE
             ON UPDATE CASCADE,
     user_id    UUID                                                     NOT NULL
-        CONSTRAINT channel_subscriptions_user_id_fk REFERENCES user_accounts
+        CONSTRAINT channel_subscriptions_user_id_fk
+            REFERENCES user_accounts
             ON DELETE CASCADE
             ON UPDATE CASCADE,
     created_at TIMESTAMP(3) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -54,7 +75,13 @@ CREATE TABLE IF NOT EXISTS public.channel_subscriptions
 CREATE UNIQUE INDEX channel_subscriptions_channel_id_user_id_key
     ON channel_subscriptions (channel_id, user_id);
 
---[ # TABLE public.channel_tiers ]--
+CREATE TRIGGER set_channel_subscriptions_updated_at
+    BEFORE UPDATE
+    ON channel_subscriptions
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at();
+
+--[ # TABLE public.channel_tiers # ]--
 
 CREATE TABLE IF NOT EXISTS public.channel_tiers
 (
@@ -80,7 +107,13 @@ CREATE UNIQUE INDEX channel_tiers_channel_id_level_key
 CREATE UNIQUE INDEX channel_tiers_channel_id_slug_key
     ON channel_tiers (channel_id, slug);
 
---[ # TABLE public.posts ]--
+CREATE TRIGGER set_channel_tiers_updated_at
+    BEFORE UPDATE
+    ON channel_tiers
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at();
+
+--[ # TABLE public.posts # ]--
 
 CREATE TABLE IF NOT EXISTS public.posts
 (
@@ -112,7 +145,13 @@ CREATE TABLE IF NOT EXISTS public.posts
 CREATE UNIQUE INDEX posts_channel_id_public_id_key
     ON posts (channel_id, public_id);
 
---[ # TABLE public.user_accounts ]--
+CREATE TRIGGER set_posts_updated_at
+    BEFORE UPDATE
+    ON posts
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at();
+
+--[ # TABLE public.user_accounts # ]--
 
 CREATE TABLE IF NOT EXISTS public.user_accounts
 (
@@ -133,7 +172,13 @@ CREATE TABLE IF NOT EXISTS public.user_accounts
 CREATE UNIQUE INDEX user_accounts_username_key
     ON user_accounts (username);
 
---[ # TABLE public.user_blocks ]--
+CREATE TRIGGER set_user_accounts_updated_at
+    BEFORE UPDATE
+    ON user_accounts
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at();
+
+--[ # TABLE public.user_blocks # ]--
 
 CREATE TABLE IF NOT EXISTS public.user_blocks
 (
@@ -154,7 +199,13 @@ CREATE TABLE IF NOT EXISTS public.user_blocks
     updated_at TIMESTAMP(3) WITHOUT TIME ZONE                           NOT NULL
 );
 
---[ # TABLE public.user_emails ]--
+CREATE TRIGGER set_user_blocks_updated_at
+    BEFORE UPDATE
+    ON user_blocks
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at();
+
+--[ # TABLE public.user_emails # ]--
 
 CREATE TABLE IF NOT EXISTS public.user_emails
 (
@@ -179,9 +230,13 @@ CREATE UNIQUE INDEX user_email_original_key
 CREATE INDEX user_email_simplified_index
     ON user_emails (simplified);
 
--- TODO Add PGSQL `genUuid` function as default for all UUID PK columns
+CREATE TRIGGER set_user_emails_updated_at
+    BEFORE UPDATE
+    ON user_emails
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at();
 
---[ # TABLE public.user_roles ]--
+--[ # TABLE public.user_roles # ]--
 
 CREATE TABLE IF NOT EXISTS public.user_roles
 (
@@ -198,7 +253,13 @@ CREATE TABLE IF NOT EXISTS public.user_roles
 CREATE UNIQUE INDEX user_role_name_key
     ON user_roles (name);
 
---[ # TABLE public.user_unlocked_posts ]--
+CREATE TRIGGER set_user_roles_updated_at
+    BEFORE UPDATE
+    ON user_roles
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at();
+
+--[ # TABLE public.user_unlocked_posts # ]--
 
 CREATE TABLE IF NOT EXISTS public.user_unlocked_posts
 (
@@ -208,14 +269,22 @@ CREATE TABLE IF NOT EXISTS public.user_unlocked_posts
     user_id    UUID                                                     NOT NULL
         CONSTRAINT user_unlocked_posts_user_id_fk
             REFERENCES user_accounts
-            ON UPDATE CASCADE ON DELETE CASCADE,
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
     post_id    UUID                                                     NOT NULL
         CONSTRAINT user_unlocked_posts_post_id_fk
             REFERENCES posts
-            ON UPDATE CASCADE ON DELETE CASCADE,
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
     created_at TIMESTAMP(3) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP(3) WITHOUT TIME ZONE                           NOT NULL
 );
 
 CREATE UNIQUE INDEX user_unlocked_posts_post_id_user_id_key
     ON user_unlocked_posts (post_id, user_id);
+
+CREATE TRIGGER set_user_unlocked_posts_updated_at
+    BEFORE UPDATE
+    ON user_unlocked_posts
+    FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_updated_at();
